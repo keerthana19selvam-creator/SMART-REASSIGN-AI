ecc9494e3284ec8edd6e025c7f6afd881d9c487d
KEERTHANA S <keerthana19selvam@gmail.com>
2026-09-09 06:36:46 -0700

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -11,7 +11,7 @@ async function analyzeAbsence(employeeId) {
     try {
 
         const response = await fetch(
-            "http://127.0.0.1:5000/reassign",
+            "https://smart-reassign-ai.onrender.com",
             {
                 method: "POST",
                 headers: {
